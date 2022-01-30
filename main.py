60ea81dc82788b50bd01353225a5fe7a758da795
Sourcery AI <>
2022-01-30 06:28:48 +0000

    'Refactored by Sourcery'

--- main.py
+++ main.py
@@ -24,24 +24,9 @@ def experiment_res(x, y):
 # User parameters for data test points
 
 # Number of test data points, tested from 3 to 5000 for subdiv=3
-n_test = 200
-
-# Number of recursive subdivisions of the initial mesh for smooth plots.
-# Values >3 might result in a very high number of triangles for the refine
-# mesh: new triangles numbering = (4**subdiv)*ntri
-subdiv = 3
-
-# Float > 0. adjusting the proportion of (invalid) initial triangles which will
-# be masked out. Enter 0 for no mask.
-init_mask_frac = 0.0
-
-# Minimum circle ratio - border triangles with circle ratio below this will be
-# masked if they touch a border. Suggested value 0.01; use -1 to keep all
-# triangles.
-min_circle_ratio = .01
-
 # Random points
 random_gen = np.random.RandomState(seed=19680801)
+n_test = 200
 x_test = random_gen.uniform(-1., 1., size=n_test)
 y_test = random_gen.uniform(-1., 1., size=n_test)
 z_test = experiment_res(x_test, y_test)
@@ -52,11 +37,13 @@ ntri = tri.triangles.shape[0]
 
 # Some invalid data are masked out
 mask_init = np.zeros(ntri, dtype=bool)
+init_mask_frac = 0.0
 masked_tri = random_gen.randint(0, ntri, int(ntri * init_mask_frac))
 mask_init[masked_tri] = True
 tri.set_mask(mask_init)
 
 
+min_circle_ratio = .01
 # ----------------------------------------------------------------------------
 # Improving the triangulation before high-res plots: removing flat triangles
 # ----------------------------------------------------------------------------
@@ -66,6 +53,7 @@ tri.set_mask(mask)
 
 # refining the data
 refiner = UniformTriRefiner(tri)
+subdiv = 3
 tri_refi, z_test_refi = refiner.refine_field(z_test, subdiv=subdiv)
 
 # analytical 'results' for comparison
@@ -74,16 +62,6 @@ z_expected = experiment_res(tri_refi.x, tri_refi.y)
 # for the demo: loading the 'flat' triangles for plot
 flat_tri = Triangulation(x_test, y_test)
 flat_tri.set_mask(~mask)
-
-
-# ----------------------------------------------------------------------------
-# Now the plots
-# ----------------------------------------------------------------------------
-# User options for plots
-plot_tri = True          # plot of base triangulation
-plot_masked_tri = True   # plot of excessively flat excluded triangles
-plot_refi_tri = False    # plot of refined triangulation
-plot_expected = False    # plot of analytical function values for comparison
 
 
 # Graphical options for tricontouring
@@ -97,18 +75,14 @@ ax.set_title("Filtering a Delaunay mesh\n"
 # 1) plot of the refined (computed) data contours:
 ax.tricontour(tri_refi, z_test_refi, levels=levels, cmap='Blues',
               linewidths=[2.0, 0.5, 1.0, 0.5])
-# 2) plot of the expected (analytical) data contours (dashed):
-if plot_expected:
+if plot_expected := False:
     ax.tricontour(tri_refi, z_expected, levels=levels, cmap='Blues',
                   linestyles='--')
-# 3) plot of the fine mesh on which interpolation was done:
-if plot_refi_tri:
+if plot_refi_tri := False:
     ax.triplot(tri_refi, color='0.97')
-# 4) plot of the initial 'coarse' mesh:
-if plot_tri:
+if plot_tri := True:
     ax.triplot(tri, color='0.7')
-# 4) plot of the unvalidated triangles from naive Delaunay Triangulation:
-if plot_masked_tri:
+if plot_masked_tri := True:
     ax.triplot(flat_tri, color='red')
 
 plt.show()
